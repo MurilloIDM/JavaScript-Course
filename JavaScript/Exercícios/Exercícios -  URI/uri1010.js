00969fbe923b82be2c405a92b5339cba3be2856f
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-07 08:36:38 -0300

    Leitura dos dados

---
+++ JavaScript/Exercícios/Exercícios -  URI/uri1010.js
@@ -0,0 +1,8 @@
+let input = require('readline-sync');
+
+let codigoPecaUm = input.questionInt();
+let quantidadePecaUm = input.questionInt();
+let valorPecaUm = input.questionFloat();
+let codigoPecaDois = input.questionInt();
+let quantidadePecaDois = input.questionInt();
+let valorPecaDois = input.questionFloat();
