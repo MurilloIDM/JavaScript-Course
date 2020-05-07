baafcb93078a896164df3d6b0316760bcac32c27
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-07 08:25:00 -0300

    Exercícios Completados URI 3

--- JavaScript/Exercícios/Exercícios -  URI/uri1009.js
+++ JavaScript/Exercícios/Exercícios -  URI/uri1009.js
@@ -1,6 +1,8 @@
 let input = require('readline-sync');
 
 let nome = input.question();
-let salario = input.questionDouble();
-let totalVendas = input.questionDouble();
+let salario = input.questionFloat();
+let totalVendas = input.questionFloat();
 
+let salarioFinal = salario + (totalVendas * 0.15);
+console.log(`TOTAL = R$ ${salarioFinal.toFixed(2)}`);
