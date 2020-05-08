84018c0d3c5e16c76cd02a46ba281818cb3fa010
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-08 10:24:36 -0300

    Exercícios Completados URI 3

--- JavaScript/Exercícios/Exercícios -  URI/uri1012.js
+++ JavaScript/Exercícios/Exercícios -  URI/uri1012.js
@@ -4,3 +4,14 @@ let a = input.questionFloat();
 let b = input.questionFloat();
 let c = input.questionFloat();
 
+let areaTriangulo = (a * c) / 2;
+let areaCirculo = 3.14159 * Math.pow(c, 2);
+let areaTrapezio = ((a + b) * c) / 2;
+let areaQuadrado = b * b;
+let areaRetangulo = a * b;
+
+console.log('TRIANGULO: ' + areaTriangulo.toFixed(3));
+console.log('CIRCULO: ' + areaCirculo.toFixed(3));
+console.log('TRAPEZIO: ' + areaTrapezio.toFixed(3));
+console.log('QUADRADO: ' + areaQuadrado.toFixed(3));
+console.log('RETANGULO: ' + areaRetangulo.toFixed(3));
