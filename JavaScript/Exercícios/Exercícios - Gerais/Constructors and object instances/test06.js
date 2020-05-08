96b42f685d77407149e29219fe95ca35ae4667d6
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-08 10:06:15 -0300

    JavaScript/Testes práticos - Constructors and object instances

--- JavaScript/Exercícios/Exercícios - Gerais/Constructors and object instances/test06.js
+++ JavaScript/Exercícios/Exercícios - Gerais/Constructors and object instances/test06.js
@@ -8,14 +8,18 @@ function Person(first, last, age, gender, interests) {
     this.gender = gender;
     this.interests = interests;
     this.bio = function() {
-        console.log(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+        if (gender.toLowerCase() == 'male') {
+            console.log(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+        } else if (gender.toLowerCase() == 'female') {
+            console.log(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. She likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+        }
     };
     this.greeting = function() {
         console.log('Hi! I\'m ' + this.name.first + '.');
     };
 }
 
-let person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiling']);
+let person1 = new Person('Maria', 'Smith', 32, 'FeMALE', ['music', 'skiling']);
 
 console.log(person1.age);
 console.log(person1.interests[1]);
