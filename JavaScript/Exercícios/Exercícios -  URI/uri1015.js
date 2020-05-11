6d70f6267925174e7794183349fb253e0424fee3
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-11 10:20:20 -0300

    Leitura dos dados

---
+++ JavaScript/Exercícios/Exercícios -  URI/uri1015.js
@@ -0,0 +1,7 @@
+//Distância entre dois pontos
+let input = require('readline-sync');
+
+let x1 = input.questionInt();
+let x2 = input.questionInt();
+let y1 = input.questionInt();
+let y2 = input.questionInt();
