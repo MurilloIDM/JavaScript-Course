cc62698f0d9bac8c88b5c23dca445e4dc1d76abe
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-11 10:09:25 -0300

    Exercícios Completados URI 4

--- JavaScript/Exercícios/Exercícios -  URI/uri1013.js
+++ JavaScript/Exercícios/Exercícios -  URI/uri1013.js
@@ -3,3 +3,7 @@ let input = require('readline-sync');
 let a = input.questionInt();
 let b = input.questionInt();
 let c = input.questionInt();
+
+let maiorAB = (a + b + Math.abs(a - b)) / 2;
+let maiorFinal = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+console.log(`${maiorFinal} eh o maior`);
