5d2acf766c675a231e6d699bbb62d0fc48b7b136
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-11 10:25:41 -0300

    Exercícios Completados URI 4

--- JavaScript/Exercícios/Exercícios -  URI/uri1015.js
+++ JavaScript/Exercícios/Exercícios -  URI/uri1015.js
@@ -2,6 +2,9 @@
 let input = require('readline-sync');
 
 let x1 = input.questionInt();
-let x2 = input.questionInt();
 let y1 = input.questionInt();
+let x2 = input.questionInt();
 let y2 = input.questionInt();
+
+let distanciaDoisPontos = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+console.log(`${distanciaDoisPontos.toFixed(4)}`);
