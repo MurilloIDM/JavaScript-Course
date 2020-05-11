69765e3fd6eb8f7b7add2875f814faec09f53863
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-11 10:16:51 -0300

    Exercícios Completados URI 4

--- JavaScript/Exercícios/Exercícios -  URI/uri1014.js
+++ JavaScript/Exercícios/Exercícios -  URI/uri1014.js
@@ -3,3 +3,6 @@ let input = require('readline-sync');
 
 let distanciaPercorrida = input.questionInt();
 let combustivelGasto = input.questionFloat();
+
+let consumoMedio = distanciaPercorrida / combustivelGasto;
+console.log(`${consumoMedio.toFixed(3)} km/l`);
