0fe6fae75347fc57b4587a7d26715eef05f3b1c6
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-22 08:20:16 -0300

    Transferencia de conteudo

--- JavaScript/Prática/Aula13/ambiente.js
+++
@@ -1,5 +0,0 @@
-var c = 1
-do {
-    console.log(c)
-    c++
-}while (c <= 6)
--- JavaScript/Prática/Aula16/função4.js
+++
@@ -1,9 +0,0 @@
-function fatorial(x) {
-    let fat = 1
-    for(let c = x; c > 1; c--) {
-        fat = fat * c
-    }
-    return fat
-}
-
-console.log(fatorial(5))
