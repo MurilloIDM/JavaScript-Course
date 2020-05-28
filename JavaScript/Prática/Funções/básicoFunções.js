1f61631c25d96a27451b233d0390e05713076211
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-28 10:07:11 -0300

    Básico sobre função

---
+++ JavaScript/Prática/Funções/básicoFunções.js
@@ -0,0 +1,13 @@
+//Funções sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(5, 4)
+
+//Funções com retorno
+function somar(a, b) {
+    return a + b
+}
+
+console.log(somar(5, 6))
