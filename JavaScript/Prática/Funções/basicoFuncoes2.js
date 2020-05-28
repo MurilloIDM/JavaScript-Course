a7608c4de6b442844f4af745c99b6846ff9970e4
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-28 10:11:06 -0300

    Básico sobre funções

---
+++ JavaScript/Prática/Funções/basicoFuncoes2.js
@@ -0,0 +1,10 @@
+//Básico de uma função anônima semr retorno
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(1, 2)
+
+//Básico função arrow function com retorno implícito
+const produto = (a, b) => console.log(a * b)
+produto(10, 2)
