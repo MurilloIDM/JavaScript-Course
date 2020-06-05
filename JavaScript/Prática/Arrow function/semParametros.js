366cb1de6f931c8f0a0dc8fc4482d26fb141130c
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-06-05 10:02:37 -0300

    Arrow function básica sem parâmetros

---
+++ JavaScript/Prática/Arrow function/semParametros.js
@@ -0,0 +1,10 @@
+//Podemos usar as arrow function sem parâmetros, para isso podemos usar os parêteses vazios
+const programadorJovem = () => {
+    return 'Hello World!'
+}
+console.log(programadorJovem())
+
+// ou
+
+const programadorJurassico = () => 'Uso assembly'
+console.log(programadorJurassico())
